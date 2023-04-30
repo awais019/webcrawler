b3cef8f2ec86250e4b709f9479b8294dab74384e
Muhammad Awais <awaistopper20@gmail.com>
2023-04-30 18:08:45 +0500

    Added striping trailing slashes.

--- crawl.js
+++ crawl.js
@@ -1,7 +1,12 @@
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
 
-  return `${urlObj.hostname}${urlObj.pathname}`;
+  const hostpath = `${urlObj.hostname}${urlObj.pathname}`;
+
+  if (hostpath.length > 0 && hostpath.slice(-1) == "/") {
+    return hostpath.slice(0, -1);
+  }
+  return hostpath;
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -10,3 +10,12 @@ test("normalizeURL strip protocol", () => {
 
   expect(actual).toEqual(expected);
 });
+
+test("normalizeURL strip trailing slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+
+  const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
