84292b0d765c66791c86b09516adf84f776deaa1
Muhammad Awais <awaistopper20@gmail.com>
2023-04-30 18:10:57 +0500

    Added tests to remove capitals and striping http.

--- crawl.test.js
+++ crawl.test.js
@@ -19,3 +19,21 @@ test("normalizeURL strip trailing slash", () => {
 
   expect(actual).toEqual(expected);
 });
+
+test("normalizeURL remove capitals", () => {
+  const input = "https://BLOG.boot.dev/path/";
+  const actual = normalizeURL(input);
+
+  const expected = "blog.boot.dev/path";
+
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+    const input = "http://blog.boot.dev/path/";
+    const actual = normalizeURL(input);
+  
+    const expected = "blog.boot.dev/path";
+  
+    expect(actual).toEqual(expected);
+  });
